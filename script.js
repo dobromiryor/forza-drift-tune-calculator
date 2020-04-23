1d576ee1ad228b30c4c25929d5464fc5bb87c9fc
Dobromir Yordanov <dobromir.yor@gmail.com>
2020-04-23 17:24:39 +0300

    Fixed calculations and styles

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-let front = 0;
-let rear = 0;
-let units = "imperial";
+let front = 50;
+let rear = 50;
 let antirollBarsFront;
 let antirollBarsRear;
+let springsMin;
+let springsMax;
 let springsFront;
 let springsRear;
-let reboundStiffnesFront;
-let reboundStiffnesRear;
+let reboundStiffnessMin;
+let reboundStiffnessMax;
+let reboundStiffnessFront;
+let reboundStiffnessRear;
+let bumpStiffnessPercent;
 let bumpStiffnessFront;
 let bumpStiffnessRear;
 
 const calculate = () => {
-  front = ($('#weightRatio').val()/100).toFixed(2);
-  rear = (1 - front).toFixed(2);
-  units = $('input[name=units]:checked').val();
+  springsMin = parseFloat($('#springsMin').val());
+  springsMax = parseFloat($('#springsMax').val());
+  reboundStiffnessMin = parseFloat($('#reboundStiffnessMin').val());
+  reboundStiffnessMax = parseFloat($('#reboundStiffnessMax').val());
+  bumpStiffnessPercent = parseFloat($('#bumpStiffnessPercent').val())/100;
+  front = parseFloat($('#weightRatio').val())/100;
+  rear = 1 - front;
 
-  if(units === "imperial") {
-    antirollBarsFront = (((65-1)*front)+1).toFixed(2);
-    antirollBarsRear = (((65-1)*rear)+1).toFixed(2);
-    springsFront = (((1524.3-304.9)*front)+304.9).toFixed(2);
-    springsRear = (((1524.3-304.9)*rear)+304.9).toFixed(2);
-    reboundStiffnesFront = (((20-3)*front)+3).toFixed(2);
-    reboundStiffnesRear = (((20-3)*rear)+3).toFixed(2);
-    bumpStiffnessFront = (reboundStiffnesFront/2).toFixed(2);
-    bumpStiffnessRear = (reboundStiffnesRear/2).toFixed(2);
-  } else if (units === "metric") {
-    antirollBarsFront = (((65-1)*front)+1).toFixed(2);
-    antirollBarsRear = (((65-1)*rear)+1).toFixed(2);
-    springsFront = (((97.5-19.5)*front)+19.5).toFixed(2);
-    springsRear = (((97.5-19.5)*rear)+19.5).toFixed(2);
-    reboundStiffnesFront = (((10-1)*front)+1).toFixed(2);
-    reboundStiffnesRear = (((10-1)*rear)+1).toFixed(2);
-    bumpStiffnessFront = (reboundStiffnesFront/2).toFixed(2);
-    bumpStiffnessRear = (reboundStiffnesRear/2).toFixed(2);
-  }
+  antirollBarsFront = ((65 - 1) * front) + 1;
+  antirollBarsRear = ((65 - 1) * rear) + 1;
+  springsFront = ((springsMax - springsMin) * front) + springsMin;
+  springsRear = ((springsMax - springsMin) * rear) + springsMin;
+  reboundStiffnessFront = ((reboundStiffnessMax - reboundStiffnessMin) * front) + reboundStiffnessMin;
+  reboundStiffnessRear = ((reboundStiffnessMax - reboundStiffnessMin) * rear) + reboundStiffnessMin;
+  bumpStiffnessFront = reboundStiffnessFront * bumpStiffnessPercent;
+  bumpStiffnessRear = reboundStiffnessRear * bumpStiffnessPercent;
   
-  $("#antirollBars").replaceWith( `<p id="antirollBars" class="light-grey-text">${antirollBarsFront}/${antirollBarsRear}</p>` )
-  $("#springs").replaceWith( `<p id="springs" class="light-grey-text">${springsFront}/${springsRear}</p>` )
-  $("#reboundStiffness").replaceWith( `<p id="reboundStiffness" class="light-grey-text">${reboundStiffnesFront}/${reboundStiffnesRear}</p>` )
-  $("#bumpStiffness").replaceWith( `<p id="bumpStiffness" class="light-grey-text">${bumpStiffnessFront}/${bumpStiffnessRear}</p>` )
+  $("#antirollBars").replaceWith( `<p id="antirollBars" class="light-grey-text fs-2 mt-1-5">${antirollBarsFront.toFixed(2)}/${antirollBarsRear.toFixed(2)}</p>` )
+  $("#springs").replaceWith( `<p id="springs" class="light-grey-text fs-2 mt-1-5">${springsFront.toFixed(2)}/${springsRear.toFixed(2)}</p>` )
+  $("#reboundStiffness").replaceWith( `<p id="reboundStiffness" class="light-grey-text fs-2 mt-1-5">${reboundStiffnessFront.toFixed(2)}/${reboundStiffnessRear.toFixed(2)}</p>` )
+  $("#bumpStiffness").replaceWith( `<p id="bumpStiffness" class="light-grey-text fs-2 mt-1-5">${bumpStiffnessFront.toFixed(2)}/${bumpStiffnessRear.toFixed(2)}</p>` )
 }
 
 $(document).ready(function(){
-  $("input[name=units]").change(calculate);
+  $("#springsMin").change(calculate);
+  $("#springsMax").change(calculate);
+  $("#reboundStiffnessMin").change(calculate);
+  $("#reboundStiffnessMax").change(calculate);
+  $("#bumpStiffnessPercent").change(calculate);
   $("#weightRatio").change(calculate);
 });
